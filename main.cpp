bec858d254e5a39d6745481a5e5be605ebccd316
Dallin Kooyman <dmkooyman@gmail.com>
2021-06-25 13:23:31 -0600

    Add Falling Animation
    This code adds the falling animation. Pieces now drop at a rate of 1 tile per .3 seconds. Additionally a animation issue was fixed. Pieces now correctly fall and rotate no matter the position including the 0 column.

--- main.cpp
+++ main.cpp
@@ -5,28 +5,28 @@
 using namespace sf;
 
 
-const std::string TILE_TEXTURE_PATH = "tiles.png";
-const int TILE_PIXEL_SIZE = 18;
-const int FIELD_WIDTH = 10;
-const int FIELD_HEIGHT = 20;
+const std::string kTileTextureFilePath = "tiles.png";
+const int kTilePixelSize = 18;
+const int kFieldWidth = 10;
+const int kFieldHeight= 20;
 
 
 
 struct Point {
     int x;
     int y;
-};
+} shape_tile_coordinates[4];
 
 //There are 7 Tetris blocks (called Tetriminos) and each have 4 blocks and a name
 //Each number represents a filled in block in a 2x4 array
-const int TERTRIMINOS[7][4] = {
-        {0,2,4,6}, //I
-        {1,3,4,5}, //J
-        {0,2,4,5}, //L
-        {0,1,2,3}, //O
-        {0,2,3,5}, //S
-        {0,2,3,4}, //T
-        {1,2,3,4}  //Z
+const int kTertriminos[7][4] = {
+        {1,3,5,7}, //I
+        {3,5,6,7}, //J
+        {2,4,6,7}, //L
+        {2,3,4,5}, //O
+        {2,4,5,7}, //S
+        {2,4,5,6}, //T
+        {3,4,5,6}  //Z
 };
 
 int main() {
@@ -34,26 +34,34 @@ int main() {
     RenderWindow window(VideoMode(320, 480), "Tetris");
 
     Texture texture;
-    texture.loadFromFile(TILE_TEXTURE_PATH);
+    texture.loadFromFile(kTileTextureFilePath);
     texture.setSmooth(true);
-    Point shapeTileCoordinates[4];
+
     Sprite sprite(texture);
     //Left X, Top Y, Num X pixels to show, Num Y pixels to show
-    sprite.setTextureRect(IntRect(0,0,TILE_PIXEL_SIZE,TILE_PIXEL_SIZE));
+    sprite.setTextureRect(IntRect(0, 0, kTilePixelSize, kTilePixelSize));
+
+    Clock clock;
 
     int dx = 0;
     bool rotate = false;
-    int colorNum = 1;
+    int color_num = 1;
+    float timer = 0;
+    float fall_delay = 0.3;
 
-    while (window.isOpen()){
+    while (window.isOpen()) {
         window.clear(Color::White);
+
+        float time = clock.getElapsedTime().asSeconds();
+        clock.restart();
+        timer += time;
 
         Event event{};
         while (window.pollEvent(event)){
-            if (event.type == Event::Closed){
+            if (event.type == Event::Closed) {
                 window.close();
             }
-            if (event.type == Event::KeyPressed){
+            if (event.type == Event::KeyPressed) {
                 switch (event.key.code) {
                     case Keyboard::Up:
                         rotate = true;
@@ -72,39 +80,47 @@ int main() {
         }
 
         //Movement
-        for (int i = 0; i < 4; i++){
-            shapeTileCoordinates[i].x += dx;
+        for (int i = 0; i < 4; i++) {
+            shape_tile_coordinates[i].x += dx;
         }
 
         //Rotation
-        if (rotate){
-            Point centerOfShape = shapeTileCoordinates[1];
-            for (int i = 0; i < 4; i++){
-                int yDistanceToCenter = shapeTileCoordinates[i].y - centerOfShape.y;
-                int xDistanceToCenter = shapeTileCoordinates[i].x - centerOfShape.x;
-                shapeTileCoordinates[i].x = centerOfShape.x - yDistanceToCenter;
-                shapeTileCoordinates[i].y = centerOfShape.y + xDistanceToCenter;
+        if (rotate) {
+            Point centerOfShape = shape_tile_coordinates[1];
+            for (int i = 0; i < 4; i++) {
+                //TODO
+                // Rename the next 2 variables to fit style guide
+                int yDistanceToCenter = shape_tile_coordinates[i].y - centerOfShape.y;
+                int xDistanceToCenter = shape_tile_coordinates[i].x - centerOfShape.x;
+                shape_tile_coordinates[i].x = centerOfShape.x - yDistanceToCenter;
+                shape_tile_coordinates[i].y = centerOfShape.y + xDistanceToCenter;
             }
         }
 
+        //Falling animation
+        if (timer > fall_delay) {
+            for (int i = 0; i < 4; i++) {
+                shape_tile_coordinates[i].y += 1;
+            }
+            timer = 0;
+        }
 
         int n=2;
-        if (shapeTileCoordinates[0].x == 0) {
-
+        if (shape_tile_coordinates[0].y == 0) {
             for (int i = 0; i < 4; i++) {
                 //Max of 2 Tiles on x-axis
-                shapeTileCoordinates[i].x = TERTRIMINOS[n][i] % 2;
+                shape_tile_coordinates[i].x = kTertriminos[n][i] % 2;
                 //Max of 4 Tiles on y-axis
-                shapeTileCoordinates[i].y = TERTRIMINOS[n][i] / 2;
+                shape_tile_coordinates[i].y = kTertriminos[n][i] / 2;
             }
         }
 
         dx = 0;
         rotate = false;
 
-        for (int i = 0; i < 4; i++){
-            sprite.setPosition(shapeTileCoordinates[i].x * TILE_PIXEL_SIZE,
-                               shapeTileCoordinates[i].y * TILE_PIXEL_SIZE);
+        for (int i = 0; i < 4; i++) {
+            sprite.setPosition(shape_tile_coordinates[i].x * kTilePixelSize,
+                               shape_tile_coordinates[i].y * kTilePixelSize);
             window.draw(sprite);
         }
         window.display();
